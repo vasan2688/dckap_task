59d48f28b52f9e2713beb24cb7d8c6b554249a43
SujanaNarasimhan <sujananarasimhan@gmail.com>
2022-04-04 09:44:39 +0530

    Revert "Revert "Revert "All code update to git"""
    This reverts commit b590fd4fba6a938455e012bd0eee6484252b94c0.

--- resources/assets/js/owl-custom.js
+++
@@ -1,1 +0,0 @@
-$('.carousel-nav').owlCarousel({items:1,loop:true,autoplay:true,nav:true});$('.carousel-dot').owlCarousel({items:1,loop:true,autoplay:true,nav:false});
--- resources/assets/pages/wall/wall.js
+++
@@ -1,2 +0,0 @@
-'use strict';$(document).ready(function(){$('#post-new').hide();$('#post-message').keyup(function(){if(($(this).val()!="")){$('#post-new').show();}else
-$('#post-new').hide();});});
